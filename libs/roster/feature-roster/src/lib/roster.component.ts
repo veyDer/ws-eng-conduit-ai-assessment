d060b332d1533ca4ef3b3c88b85647ad4f5ca3f6
Bart Rudzki <veyder@gmail.com>
2023-09-25 15:04:06 +1300

    feat: sort the roster by likes descending

--- libs/roster/feature-roster/src/lib/roster.component.ts
+++ libs/roster/feature-roster/src/lib/roster.component.ts
@@ -3,6 +3,7 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { rosterActions, selectData, selectRosterState } from '@realworld/roster/data-access';
 import { Store } from '@ngrx/store';
 import { CommonModule } from '@angular/common';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'cdt-roster',
@@ -16,7 +17,11 @@ import { CommonModule } from '@angular/common';
 @UntilDestroy()
 export class RosterComponent implements OnInit {
   // add roster variable and pipe it to untilDestroyed
-  roster$ = this.store.select(selectRosterState).pipe(untilDestroyed(this));
+  roster$ = this.store.select(selectRosterState).pipe(
+    untilDestroyed(this),
+    // add the mapping from state to a copy of state.data.stats sorted by likesReceivedOnArticlesCount descending
+    map((state) => [...state.data.stats].sort((a, b) => b.likesReceivedOnArticlesCount - a.likesReceivedOnArticlesCount))
+  );
 
   constructor(private readonly store: Store) {}
 

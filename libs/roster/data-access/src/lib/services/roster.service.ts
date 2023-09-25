2d9d424df4b196d469cde3fb7ba81f8af027b3d9
Bart Rudzki <veyder@gmail.com>
2023-09-25 15:00:34 +1300

    fix: update types and imports

--- libs/roster/data-access/src/lib/services/roster.service.ts
+++ libs/roster/data-access/src/lib/services/roster.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiService } from '@realworld/core/http-client';
-import { RosterState } from '../models/roster.model';
+import { Roster } from '@realworld/core/api-types';
 
-// generate `RosterService` with the `getRoster()` method returning `Observable<RosterState>`
+// generate `RosterService` with the `getRoster()` method returning `Observable<Roster>`
 @Injectable({ providedIn: 'root' })
 export class RosterService {
   constructor(private apiService: ApiService) {}
 
-  getRoster(): Observable<RosterState> {
-    return this.apiService.get<RosterState>('/roster');
+  getRoster(): Observable<Roster> {
+    return this.apiService.get<Roster>('/roster');
   }
 }
